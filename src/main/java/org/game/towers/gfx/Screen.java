36d0c6906eb529b9bd04e62ccd506c8d10990c55
eliotik <eliotik@gmail.com>
2014-01-13 23:41:14 +0200

    work with screen and colors;

--- src/main/java/org/game/towers/gfx/Screen.java
+++ src/main/java/org/game/towers/gfx/Screen.java
@@ -7,8 +7,9 @@ public class Screen {
 	public static final int MAP_WIDTH = 64;
 	public static final int MAP_WIDTH_MASK = MAP_WIDTH - 1;
 	
-	public int[] tiles = new int[MAP_WIDTH * MAP_WIDTH];
-	public int[] colors = new int[MAP_WIDTH * MAP_WIDTH * 4];
+//	public int[] tiles = new int[MAP_WIDTH * MAP_WIDTH];
+	
+	public int[] pixels;
 	
 	public int xOffset = 0;
 	public int yOffset = 0;
@@ -22,41 +23,29 @@ public class Screen {
 		this.width = width;
 		this.height = height;
 		this.sheet = sheet;
-		System.out.println(MAP_WIDTH * MAP_WIDTH - 3300);
-		for (int i = 0; i < MAP_WIDTH * MAP_WIDTH - 3300; i++) {
-			colors[i*4+0] = 0xff00ff;
-			colors[i*4+1] = 0x00ffff;
-			colors[i*4+2] = 0xffff00;
-			colors[i*4+3] = 0xffffff;
-		}
+		
+		pixels = new int[width * height];
 	}
 
-	public void render(int[] pixels, int offset, int row) {
-		for(int yTile = yOffset >> 3; yTile <= (yOffset + height) >> 3; yTile++) {
-			int yMin = yTile * 8 - yOffset;
-			int yMax = yMin + 8;
+	public void render(int xPos, int yPos, int tile, int color, boolean mirrorX, boolean mirrorY) {
+		xPos -= xOffset;
+		yPos -= yOffset;
+		
+		int xTile = tile % 32;
+		int yTile = tile / 32;
+		int tileOffset = (xTile << 3) + (yTile << 3) * sheet.getWidth();
+		
+		for (int y = 0; y < 8; y++) {
+			int ySheet = y;
+			if (mirrorY) ySheet = 7 - y;
+			if (y + yPos < 0 || y + yPos >= height) continue;
 			
-			if (yMin < 0) yMin = 0;
-			if (yMax > height) yMax = height;
-			
-			for(int xTile = xOffset >> 3; xTile <= (xOffset + width) >> 3; xTile++) {
-				int xMin = xTile * 8 - xOffset;
-				int xMax = xMin + 8;
-				
-				if (xMin < 0) xMin = 0;
-				if (xMax > width) xMax = width;
-				
-				int tileIndex = (xTile & (MAP_WIDTH_MASK)) + (yTile & (MAP_WIDTH_MASK)) * MAP_WIDTH;
-				
-				for (int y = yMin; y < yMax; y++) {
-					int sheetPixel = ((y + yOffset) & 7) * sheet.getWidth() + ((xMin + xOffset) & 7);
-					int tilePixel = offset + xMin + y * row;
-					
-					for (int x = xMin; x < xMax; x++) {
-						int color = tileIndex * 4 + sheet.getPixels()[sheetPixel++];
-						pixels[tilePixel++] = colors[color];
-					}
-				}
+			for (int x = 0; x < 8; x++) {
+				int xSheet = x;
+				if (mirrorX) xSheet = 7 - x;
+				if (x + xPos < 0 || x + xPos >= width) continue;
+				int col = (color >> (sheet.getPixels()[xSheet + ySheet * sheet.getWidth() + tileOffset] * 8)) & 255;
+				if (col < 255) pixels[(x + xPos) + (y + yPos) * width] = col;
 			}
 		}
 	}
