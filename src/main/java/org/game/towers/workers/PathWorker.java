a3b6783e2577f878002d45f4a949263bf009d647
arhitiron <tiron_alexey@mail.ru>
2014-01-27 18:24:10 +0200

    pathworker changed

--- src/main/java/org/game/towers/workers/PathWorker.java
+++ src/main/java/org/game/towers/workers/PathWorker.java
@@ -25,16 +25,6 @@ public class PathWorker {
         return isAvailable;
     }
 
-//    public Cell getNextCell(Cell currentCell) {
-//
-////        List<Cell> neighborList = getEmptyNeighbor(currentCell);
-////        for(Cell neighbor : neighborList) {
-////
-////        }
-//        Cell cell = new Cell(0,0);
-//        return cell;
-//    }
-
     private int roundDownScale(double aValue)
     {
         BigDecimal decimal = new BigDecimal(aValue);
@@ -42,28 +32,6 @@ public class PathWorker {
         int result = decimal.intValue();
         return result;
     }
-
-//    private Cell hasBarrier(Cell startCell, Cell finishCell){
-//        int xStart = startCell.getGridX();
-//        int yStart = startCell.getGridY();
-//        int xFinish = finishCell.getGridX();
-//        int yFinish = finishCell.getGridY();
-//        int checkX, checkY;
-//        double deltaX = (xFinish - xStart) / (2 * Config.AMOUNT_HORIZONTAL_PIX);
-//        double deltaY = (yFinish - yStart) / (2 * Config.AMOUNT_VERTICAL_PIX);
-//
-//        for (double x = xStart; x < xFinish; x = x + deltaX) {
-//            for (double y = yStart; y < yFinish; y = y + deltaY) {
-//                checkX = roundDownScale(x);
-//                checkY = roundDownScale(y);
-//                if (Game.grid.cells[checkX][checkY].getType() == null){
-//                    Cell barrierCell = Game.grid.cells[checkX][checkY];
-//                    return barrierCell;
-//                }
-//            }
-//        }
-//        return null;
-//    }
 
     private Level.TileMap getBarrier(int xStart, int xFinish, int yStart, int yFinish){
         HashMap<Integer, Level.TileMap> temporaryTileMap = Game.instance.getWorld().getLevel().getTiles();
@@ -153,16 +121,16 @@ public class PathWorker {
 //        return null;
     }
 
-    public Dimension getNextCoordinates(int x, int y){
-        int finishX = Level.Portals.getExit().getX();
-        int finishY = Level.Portals.getExit().getY();
-        Level.TileMap barrier = getBarrier(x, finishX, y, finishY);
-        double deltaX = (finishX - x);
-        double deltaY = (finishY - y);
-        Dimension dimension = new Dimension();
-
-        return dimension;
-    }
+//    public Dimension getNextCoordinates(int x, int y){
+//        int finishX = Level.Portals.getExit().getX();
+//        int finishY = Level.Portals.getExit().getY();
+//        Level.TileMap barrier = getBarrier(x, finishX, y, finishY);
+//        double deltaX = (finishX - x);
+//        double deltaY = (finishY - y);
+//        Dimension dimension = new Dimension();
+//
+//        return dimension;
+//    }
 
     private int getLogicZone(int x, int y, Level.TileMap construction){
         if (x < construction.getGeo().getTopLeft().getX() && y < construction.getGeo().getTopLeft().getY()) {
@@ -261,83 +229,28 @@ public class PathWorker {
         return secondPotentialPoint;
     }
 
+    private int getShift(int dimension1, int dimension2) {
+        int deltaDim = dimension2 - dimension1;
 
+        if (deltaDim > 0){
+            return 1;
+        }
+        if (deltaDim < 0){
+            return -1;
+        }
 
-
-//    private List<Cell> getRectangleApexCell(Cell cell) {
-//        List<Cell> apex = new ArrayList<Cell>();
-//        Rectangle size = cell.getType().getGeo();
-//        double unitX = size.getX();
-//        double unitY = size.getY();
-//        double unitWidth = size.getWidth();
-//        double unitHeight = size.getHeight();
-//
-//        Cell apexCellA = Game.grid.cells[(int)unitX - 1][(int)unitY - 1];
-//        Cell apexCellB = Game.grid.cells[(int)unitX + (int)unitWidth + 1][(int)unitY - 1];
-//        Cell apexCellC = Game.grid.cells[(int)unitX + (int)unitWidth + 1][(int)unitY + (int)unitHeight + 1];
-//        Cell apexCellD = Game.grid.cells[(int)unitX + (int)unitWidth - 1][(int)unitY + (int)unitHeight + 1];
-//
-//        apex.add(apexCellA);
-//        apex.add(apexCellB);
-//        apex.add(apexCellC);
-//        apex.add(apexCellD);
-//
-//        return apex;
-//    }
-
-//    private List<Cell> checkNeighborUnitCell(Cell cell){
-//        List<Cell> listNeighborUnitCell = new ArrayList<Cell>();
-//        int towerWidth = (int)cell.getType().getGeo().getWidth();
-//        int towerHeight = (int)cell.getType().getGeo().getHeight();
-//        int towerX = cell.getType().getTileX(); // если возвращает положение строения, то правильно
-//        int towerY = cell.getType().getTileY(); // если возвращает положение строения, то правильно
-//
-//        if (towerX > 5 && towerY > 5) {
-//            for (int x = towerX - 6; x < towerX + towerWidth + 6; x++) {
-//                for (int y = towerY - 6; y < towerY + towerHeight + 6; y++) {
-//                    if (Game.grid.cells[x][y] != null) {
-//                        listNeighborUnitCell.add(Game.grid.cells[x][y]);
-//                    }
-//                }
-//            }
-//            return listNeighborUnitCell;
-//        }
-//        return null;
-//    }
-
-//    private List<Cell> getMainRectangleApexCell(List<Cell> apex, Cell startCell) {
-//        List<Cell> mainApex = new ArrayList<Cell>();
-//        for(Cell apexItem : apex) {
-//            if (hasBarrier(startCell, apexItem) == null){
-//                mainApex.add(apexItem);
-//            }
-//        }
-//        return mainApex;
-//    }
-//
-//    private List<Cell> getBorderCells(Cell cell) {
-//        List<Cell> borders = new ArrayList<Cell>();
-//
-//        return borders;
-//    }
-//
-//    private List<Cell> getEmptyNeighbor(Cell currentCell) {
-//        List<Cell> neighborList = new ArrayList<Cell>();
-//
-//        for(int x = currentCell.getGridX() - 1; x <= currentCell.getGridX() + 1; x++ ) {
-//            for(int y = currentCell.getGridY() - 1; y <= currentCell.getGridY() + 1; y++ ) {
-//                if (currentCell.getType() == null) {
-//                    neighborList.add(Game.grid.cells[x][y]);
-//                }
-//            }
-//        }
-//        return neighborList;
-//    }
-
-    private Unit getCollision() {
-        List<Unit> units = Game.grid.units;
-
-
-        return null;
+        return 0;
     }
+
+    public void nextCoordinate(int x, int y, Point point) {
+        int finishX = Level.Portals.getExit().getX();
+        int finishY = Level.Portals.getExit().getY();
+        Level.TileMap barrier = getBarrier(x, finishX, y, finishY);
+        Coordinates coordinate = getTransitionalFinish(x, y, barrier);
+        getShift();
+    }
+
+
+
+
 }
