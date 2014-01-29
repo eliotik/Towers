e5fcfe0e0ffdaeb15dc5970bd72162fe5de7c934
eliotik <eliotik@gmail.com>
2014-01-29 19:04:13 +0200

    refactoring spritesdata

--- src/main/java/org/game/towers/gfx/sprites/SpritesData.java
+++ src/main/java/org/game/towers/gfx/sprites/SpritesData.java
@@ -7,7 +7,10 @@ public class SpritesData {
 			new BlockSpriteSheet(Config.SPRITESHEET_FILE),
 			new NpcSpriteSheet(Config.SPRITESHEET_NPCS_FILE) };
 
-	private static Sprite[] sprites = new Sprite[256];
+	public static Sprite GRASS = new Sprite("GRASS", 0, 0, 16, 16, "BLOCK");
+	public static Sprite SAND = new Sprite("SAND", 1, 0, 16, 16, "BLOCK");
+	public static Sprite STONE = new Sprite("STONE", 2, 0, 16, 16, "BLOCK");
+	public static Sprite BUSH = new Sprite("BUSH", 3, 0, 16, 16, "BLOCK");
 
 	public static SpriteSheet[] getSpriteSheets() {
 		return spriteSheets;
@@ -15,14 +18,6 @@ public class SpritesData {
 
 	public static void setSpriteSheets(SpriteSheet[] s) {
 		spriteSheets = s;
-	}
-
-	public static Sprite[] getSprites() {
-		return sprites;
-	}
-
-	public static void setSprites(Sprite[] s) {
-		sprites = s;
 	}
 
 	public static SpriteSheet getSpriteSheet(String type) {
