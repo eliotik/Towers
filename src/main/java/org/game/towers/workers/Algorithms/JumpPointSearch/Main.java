8e84b13be46e8288dcb4aafa1cffe746e93e3278
arhitiron <tiron_alexey@mail.ru>
2014-02-05 11:41:50 +0200

    added algorithms

---
+++ src/main/java/org/game/towers/workers/Algorithms/JumpPointSearch/Main.java
@@ -0,0 +1,16 @@
+package org.game.towers.workers.Algorithms.JumpPointSearch;
+
+public class Main {
+	public static void main(String[] args){
+		
+		int xMax = 1000;  //size of grid x direction
+		int yMax = 1000;  //size of the grid y direction
+		int xIsland = 1; //islands along the x direction
+		int yIsland = 1; //islands along the y direction
+		boolean uniform = true; //true for uniform land generation, false for random land generation
+		boolean draw = true;
+		Node[][] grid = null;
+		
+		JPS jpsg = new JPS(xMax,yMax,xIsland,yIsland,uniform,draw,grid);
+	}
+}
