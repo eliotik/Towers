81705e0abacf18ae3d952ba2ceab40305d5e4c21
arhitiron <tiron_alexey@mail.ru>
2014-02-13 18:15:04 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/java/org/game/towers/workers/PathWorker.java
    	src/resources/configs/npcs.xml
    
    worked pathworker

--- src/main/java/org/game/towers/workers/PathWorker.java
+++ src/main/java/org/game/towers/workers/PathWorker.java
@@ -55,17 +55,6 @@ public class PathWorker {
         Game.instance.getWorld().getLevel().getTile(nextNode.getX()>>4, nextNode.getY()>>4).setHighlight(0.8);
         int nextX = Game.instance.getWorld().getLevel().getTile(nextNode.getX()>>4, nextNode.getY()>>4).getX()*Config.BOX_SIZE;
         int nextY = Game.instance.getWorld().getLevel().getTile(nextNode.getX()>>4, nextNode.getY()>>4).getY()*Config.BOX_SIZE;
-//        if (Game.instance.getWorld().getLevel().getUnits().get(0).getNumSteps() - steps >= 10) {
-//	        System.out.println(">>>>====================");
-////	    	System.out.println("tile x: "+tile.getX()+", nextnode x: "+nextNode.getX()+", npc x: "+ x+", tile x + boxsize/2: "+ (tile.getX()-Config.BOX_SIZE_FIXED - Config.BOX_SIZE_FIXED/2));
-////	    	System.out.println("tile y: "+tile.getY()+", nextnode y: "+nextNode.getY()+", npc y: "+ y+", tile y + boxsize/2: "+ (tile.getY()-Config.BOX_SIZE_FIXED - Config.BOX_SIZE_FIXED/2));
-//            System.out.println("x: "+x+ " nextX: " + (nextX) + " y: " + y + " nextY: " + (nextY));
-////            System.out.println("visitedNodes.size() " + visitedNodes.size());
-//            System.out.println("<<<<====================");
-//	    	steps = Game.instance.getWorld().getLevel().getUnits().get(0).getNumSteps();
-//        }
-//        if ( ((tile.getX()-Config.BOX_SIZE_FIXED) - Config.BOX_SIZE_FIXED/2) == x && ((tile.getY()-Config.BOX_SIZE_FIXED) - Config.BOX_SIZE_FIXED/2) == y ) {
-//    	if ((nextNode.getX() - Config.BOX_SIZE_FIXED) == x && (nextNode.getY() - Config.BOX_SIZE_FIXED) == y) {
         if (nextX == x && nextY == y) {
             visitedNodes.add(nextNode);
             visitedNodesMap.put(id, visitedNodes);
