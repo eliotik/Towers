b6190d8ebb0fb1b3d7332548032ac15773948959
arhitiron <tiron_alexey@mail.ru>
2014-03-09 14:52:46 +0200

    wave with random units type

--- src/main/java/org/game/towers/workers/Algorithms/MathAlgorithms/Circle.java
+++ src/main/java/org/game/towers/workers/Algorithms/MathAlgorithms/Circle.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 class Circle {
     private Coordinates coordinates;
     private int radius;
-    private Coordinates[] circleEqual;
+    private Coordinates[][] circleEqual;
     int minX, maxX, minY, maxY;
 
     public Circle(Coordinates coordinatesCenter, int radius) {
@@ -26,7 +26,7 @@ class Circle {
             for (int y = minY; y <= maxY; y++) {
                 if (java.lang.Math.pow((double)(x - coordinates.getX()), 2) +
                     java.lang.Math.pow((double)(y - coordinates.getX()), 2) == java.lang.Math.pow((double)radius, 2)) {
-                    circleEqual[y] = new Coordinates(x, y);
+                    circleEqual[x][y] = new Coordinates(x, y);
                 }
             }
         }
@@ -34,9 +34,23 @@ class Circle {
 
     public HashMap<Coordinates, Integer> getInscribedCoordinates(){
         circleEqual();
+        int fromY = 0;
+        int toY = 0;
         HashMap<Coordinates, Integer> inscribedCoordinates = new HashMap<Coordinates, Integer>();
-        for (int y = minY; y <= maxY; y++) {
-            inscribedCoordinates.put(circleEqual[y], 0);
+        for (int x = minX; x <= maxX; x++) {
+            if (circleEqual[x].length > 1) {
+                for(Coordinates coordinate : circleEqual[x]) {
+                    if (toY < coordinate.getY()) {
+                        toY = coordinate.getY();
+                    } else {
+                        fromY = coordinate.getY();
+                    }
+                }
+                for (int coordY = fromY; coordY <= toY; coordY++) {
+                    inscribedCoordinates.put(new Coordinates(x, coordY), 0);
+                }
+            }
+
         }
         return inscribedCoordinates;
     }
