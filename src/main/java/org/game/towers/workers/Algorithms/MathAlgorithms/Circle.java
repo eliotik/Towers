7cafe9f8b49d4f59518ec0b278689f95644fefd9
arhitiron <tiron_alexey@mail.ru>
2014-03-09 16:50:24 +0200

    getting points in a circle

--- src/main/java/org/game/towers/workers/Algorithms/MathAlgorithms/Circle.java
+++ src/main/java/org/game/towers/workers/Algorithms/MathAlgorithms/Circle.java
@@ -1,32 +1,48 @@
 package org.game.towers.workers.Algorithms.MathAlgorithms;
 
 
+import ch.lambdaj.Lambda.*;
 import org.game.towers.geo.Coordinates;
+import org.hamcrest.Matchers;
 
 import java.lang.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+
+import static ch.lambdaj.Lambda.filter;
+import static ch.lambdaj.Lambda.having;
+import static ch.lambdaj.Lambda.on;
+
 
 class Circle {
     private Coordinates coordinates;
     private int radius;
-    private Coordinates[][] circleEqual;
     int minX, maxX, minY, maxY;
+    private ArrayList<Coordinates> circle = new ArrayList<Coordinates>();
+    private Coordinates[][] circleEqual;
+    private double epsilon = 0.1;
+
 
     public Circle(Coordinates coordinatesCenter, int radius) {
         this.coordinates = coordinatesCenter;
         this.radius = radius;
-        minX = coordinates.getX() - radius;
-        maxX = coordinates.getX() + radius;
-        minY = coordinates.getY() - radius;
-        maxY = coordinates.getY() + radius;
+        this.minX = coordinates.getX() - radius;
+        this.maxX = coordinates.getX() + radius;
+        this.minY = coordinates.getY() - radius;
+        this.maxY = coordinates.getY() + radius;
     }
 
     private void circleEqual(){
         for(int x = minX; x <= maxX; x++) {
             for (int y = minY; y <= maxY; y++) {
-                if (java.lang.Math.pow((double)(x - coordinates.getX()), 2) +
-                    java.lang.Math.pow((double)(y - coordinates.getX()), 2) == java.lang.Math.pow((double)radius, 2)) {
-                    circleEqual[x][y] = new Coordinates(x, y);
+                double radiusVector  = java.lang.Math.pow((double)(x - coordinates.getX()), 2) +
+                                       java.lang.Math.pow((double)(y - coordinates.getY()), 2);
+
+                if ((radiusVector / java.lang.Math.pow((double)radius, 2)) < 1 + epsilon &&
+                    (radiusVector / java.lang.Math.pow((double)radius, 2)) > 1 - epsilon) {
+                    circle.add(new Coordinates(x, y));
                 }
             }
         }
@@ -38,20 +54,22 @@ class Circle {
         int toY = 0;
         HashMap<Coordinates, Integer> inscribedCoordinates = new HashMap<Coordinates, Integer>();
         for (int x = minX; x <= maxX; x++) {
-            if (circleEqual[x].length > 1) {
-                for(Coordinates coordinate : circleEqual[x]) {
-                    if (toY < coordinate.getY()) {
-                        toY = coordinate.getY();
-                    } else {
-                        fromY = coordinate.getY();
-                    }
+            List<Coordinates> listCoordinates = filter(having(on(Coordinates.class).getX(), Matchers.equalTo(x)), circle);
+            fromY = listCoordinates.get(0).getY();
+            toY = listCoordinates.get(1).getY();
+            for(Coordinates item : listCoordinates){
+                if (fromY >  item.getY()) {
+                    fromY = item.getY();
                 }
-                for (int coordY = fromY; coordY <= toY; coordY++) {
-                    inscribedCoordinates.put(new Coordinates(x, coordY), 0);
+                if (toY < item.getY()){
+                    toY = item.getY();
                 }
             }
-
+            for (int y = fromY; y <= toY; y++) {
+                inscribedCoordinates.put(new Coordinates(x, y), 0);
+            }
         }
+
         return inscribedCoordinates;
     }
 
--- src/main/java/org/game/towers/workers/Algorithms/MathAlgorithms/MathAlgorithms.java
+++ src/main/java/org/game/towers/workers/Algorithms/MathAlgorithms/MathAlgorithms.java
@@ -8,11 +8,21 @@ public class MathAlgorithms {
 
     public MathAlgorithms() {}
 
-    public HashMap<Coordinates, Integer> getInscribedCoordinates(Coordinates coordinatesCenter, int radius) {
+    public static HashMap<Coordinates, Integer> getInscribedCoordinates(double x, double y, int radius) {
+        Coordinates coordinatesCenter = new Coordinates((int)x, (int)y);
+        Circle circle = new Circle(coordinatesCenter, radius);
+
+        return circle.getInscribedCoordinates();
+    }
+
+
+    // test
+    public static void main(String[] args) {
+        Coordinates coordinatesCenter = new Coordinates(100, 100);
+        int radius = 10;
         Circle circle = new Circle(coordinatesCenter, radius);
         HashMap<Coordinates, Integer> inscribedCoordinates = circle.getInscribedCoordinates();
-
-        return inscribedCoordinates;
+        System.out.println(inscribedCoordinates.size());
     }
 
 }
